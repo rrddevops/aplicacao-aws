bcf1737dae2003a1fd1e815e1d1b909347ed99fb
rodrigordavila@gmail.com <rodrigordavila@gmail.com>
2022-06-21 12:06:38 -0300

    versao2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          App React
+          App React JS
         </p>
         <a
           className="App-link"
