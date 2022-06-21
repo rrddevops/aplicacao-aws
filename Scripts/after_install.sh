3cea9ceb267efc0a7ebe488973497dde1b57daf9
rodrigordavila@gmail.com <rodrigordavila@gmail.com>
2022-06-21 11:34:15 -0300

    versao1

---
+++ Scripts/after_install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo mv /home/ec2-user/app/* /var/www/html/   
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          App React
+        </p>
+        <a
+          className="App-link"
+          href="http://loglan.com.br"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Rodrigo Loglan
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
